2e7fabbf918b829716c35823302c9885df3e16a0
ang3lmira <meeraolumudi@icloud.com>
2022-09-03 23:21:34 +0100

    update 2

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,24 +8,12 @@ screen.bgcolor("black")
 screen.title("m's snake game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        segments[seg_num - 1].xcor()
-        segments[seg_num - 1].ycor()
-        segments[seg_num].goto()
-    segments[0].fd(20)
 screen.exitonclick()
