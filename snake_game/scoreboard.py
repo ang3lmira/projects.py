d83e43be8e4aa3de4eff7646891680961a9e2b7b
ang3lmira <meeraolumudi@icloud.com>
2022-09-07 21:04:26 +0100

    update 1

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -11,22 +11,25 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
+        self.high_score = 0
         self.hideturtle()
-        self.write(f"Score: {self.score}", align="center",
-                   font=("Calibri", 24, "normal"))
+        self.write(
+            f"Score: {self.score} High Score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
         self.update_score
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT,
+        self.clear()
+        self.write(f"Score: {self.score} High Score:{self.high_score}", align=ALIGNMENT,
                    font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT,
-                   font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
+        # self.clear()
         self.update_score()
