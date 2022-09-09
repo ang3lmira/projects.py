5d879bca396cc971443aa6d67ed33ea025fc99d9
ang3lmira <meeraolumudi@icloud.com>
2022-09-09 13:54:17 +0100

    update 2

--- oop_day9/Mail Merge Project Start/main.py
+++ oop_day9/Mail Merge Project Start/main.py
@@ -9,7 +9,7 @@
 
 PLACEHOLDER = "[name]"
 
-with open("./Input/Names/invited_names.txt") as names_file:
+with open(file="./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
 with open("./Input/Letters/starting_letter.txt") as letter_file:
