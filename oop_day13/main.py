1c9f040e4390bf109e63b379a90082f52622ba5c
ang3lmira <meeraolumudi@icloud.com>
2022-09-14 18:56:20 +0100

    update 1

--- oop_day13/main.py
+++ oop_day13/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,7 +14,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start():
+    countdown(5*60)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def countdown(count):
+    minute = math.floor(count / 60)
+    seconds = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{minute}:{seconds}")
+    if count > 0:
+        window.after(1000, countdown, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -30,22 +44,25 @@ window = Tk()
 window.title("m's pomodoro")
 window.config(padx=100, pady=50, bg=PINK)
 
+
 canvas = Canvas(width=200, height=224, bg=PINK, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
+                                font=(FONT_NAME, 35, "bold"))
 
 canvas.grid(row=1, column=1)
 
-timer_label = Label(font=(FONT_NAME, 20), text="Timer",
-                    fg=GREEN)
+
+timer_label = Label(font=(FONT_NAME, 50), text="Timer", bg=PINK, fg=GREEN)
 timer_label.grid(row=0, column=1)
 
-start_button = Button(text="Start", command=start_count)
+start_button = Button(text="Start", command=start, highlightthickness=0)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", command=reset_count)
+reset_button = Button(text="Reset", command=reset_count, highlightthickness=0)
 reset_button.grid(row=2, column=2)
 
+check_marks = Label(text="✓", fg=GREEN, bg=PINK)
+check_marks.grid(row=3, column=1)
 window.mainloop()
