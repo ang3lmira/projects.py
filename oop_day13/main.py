5c0440d15c06731b09d545c36237b046f4e54bd1
ang3lmira <meeraolumudi@icloud.com>
2022-09-14 20:08:02 +0100

    update 2

--- oop_day13/main.py
+++ oop_day13/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 import math
+timer = None
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,14 +11,35 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- #
 
+
+def reset():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_label, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text=" ")
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start():
-    countdown(5*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+
+    if reps % 8 == 0:
+        countdown(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        countdown(short_break_sec)
+        timer_label.config(text="Break", fg=YELLOW)
+    else:
+        countdown(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -25,19 +48,22 @@ def countdown(count):
     minute = math.floor(count / 60)
     seconds = count % 60
 
+    if seconds < 10:
+        seconds = (f"0{seconds}")
+
     canvas.itemconfig(timer_text, text=f"{minute}:{seconds}")
     if count > 0:
-        window.after(1000, countdown, count-1)
+        global timer
+        timer = window.after(1000, countdown, count-1)
+    else:
+        start()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_marks.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
-def start_count():
-    pass
-
-
-def reset_count():
-    pass
 
 
 window = Tk()
@@ -60,9 +86,9 @@ timer_label.grid(row=0, column=1)
 start_button = Button(text="Start", command=start, highlightthickness=0)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", command=reset_count, highlightthickness=0)
+reset_button = Button(text="Reset", command=reset, highlightthickness=0)
 reset_button.grid(row=2, column=2)
 
-check_marks = Label(text="✓", fg=GREEN, bg=PINK)
+check_marks = Label(fg=GREEN, bg=PINK)
 check_marks.grid(row=3, column=1)
 window.mainloop()
